ff1a22491f9b7a2f36599603591649cf6393df20
Juan Manuel Cantero Diego <juanmanuelcantero@gmail.com>
2020-10-14 00:23:19 +0200

    first day: created some views, adding some config and a navbar for the home page

---
+++ application/views/templates/header.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>ciBlog</title>
+        <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
+    </head>
+    <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="navbar-header">  
+    <a class="navbar-brand" href="/">ciBlog</a>
+  </div>
+  <div class="collapse navbar-collapse" id="navbarColor03">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/about">About</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+        
