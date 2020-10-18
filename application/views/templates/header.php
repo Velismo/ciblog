918ce3c4380330a51588dbad551038f951c45dab
Juan Manuel Cantero Diego <juanmanuelcantero@gmail.com>
2020-10-18 14:07:12 +0200

    adding a new folder for the posts of the blog

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,19 +4,21 @@
         <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
     </head>
     <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <div class="navbar-header">  
     <a class="navbar-brand" href="/">ciBlog</a>
   </div>
   <div class="collapse navbar-collapse" id="navbarColor03">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="/">Home</a>
+        <a class="nav-link" href="<?php echo base_url(); ?>">Home</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/about">About</a>
+        <a class="nav-link" href="<?php echo base_url(); ?>/about">About</a>
       </li>
     </ul>
   </div>
 </nav>
+
+<div class="container">
         
