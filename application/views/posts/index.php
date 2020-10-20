bf24b03c10d34fd51cbe1a6e875a3457e7b34aac
Juan Manuel Cantero Diego <juanmanuelcantero@gmail.com>
2020-10-20 22:00:57 +0200

    adding the database and the posts

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,1 +1,7 @@
-<h2><?= $title ?></h2>
+<br>
+<h2><?= $title ?></h2><br>
+<?php foreach($posts as $post) : ?>
+    <h3><?php echo $post['title']; ?></h3>
+    <small class="post-date">Posted on: <?php echo $post['created_at']; ?></small><br>
+    <?php echo $post['body']; ?><br>
+<?php endforeach; ?> 
