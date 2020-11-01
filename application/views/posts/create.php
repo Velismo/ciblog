964bc98f8e4defc7a36f68e6f8548825a8df4d4e
Juan Manuel Cantero Diego <juanmanuelcantero@gmail.com>
2020-11-01 20:31:32 +0100

    finished create create posts

--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -1,17 +1,15 @@
 <h2><?= $title; ?></h2>
-<form>
+
+<?php echo validation_errors(); ?>
+
+<?php echo form_open('posts/create'); ?>
   <div class="form-group">
     <label>Title</label>
     <input type="text" class="form-control" name="title" placeholder="Add Title">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group">
     <label>Body</label>
-    <textarea type="password" class="form-control" id="exampleInputPassword1"></textarea>
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    <textarea class="form-control" name="body" placeholder="Add Body"></textarea>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
