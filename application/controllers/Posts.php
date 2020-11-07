0a175d5225bdca41e0357131e07e3dd6b50fb1de
Juan Manuel Cantero Diego <juanmanuelcantero@gmail.com>
2020-11-07 12:17:17 +0100

    adding some new stuffs

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -27,6 +27,8 @@
         public function create(){
             $data['title'] = 'Create Post';
 
+            $data['categories'] = $this->post_model->get_categories();
+
             $this->form_validation->set_rules('title', 'Title', 'required');
             $this->form_validation->set_rules('body', 'Body', 'required');
 
@@ -38,5 +40,29 @@
                 $this->post_model->create_post();
                 redirect('posts');
             }
+        }
+
+        public function delete($id){
+            $this->post_model->delete_post($id);
+            redirect('posts');
+        }
+
+        public function edit($slug){
+            $data['post'] = $this->post_model->get_posts($slug);
+
+            if(empty($data['post'])){
+                show_404();
+            }
+
+            $data['title'] = 'Edit Post';
+
+            $this->load->view('templates/header');
+            $this->load->view('posts/edit', $data);
+            $this->load->view('templates/footer');
+        }
+
+        public function update(){
+            $this->post_model->update_post();
+            redirect('posts');
         }
     }
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,6 +3,7 @@
         <title>ciBlog</title>
         <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
         <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css">
+        <script src="https://cdn.ckeditor.com/ckeditor5/23.1.0/classic/ckeditor.js"></script>
     </head>
     <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -11,16 +12,22 @@
   </div>
   <div class="collapse navbar-collapse" id="navbarColor03">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
+      <li class="nav-item">
         <a class="nav-link" href="<?php echo base_url(); ?>">Home</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="<?php echo base_url(); ?>about">About</a>
       </li>
-      <li class="nav-item active">
+      <li class="nav-item">
         <a class="nav-link" href="<?php echo base_url(); ?>posts">Blog</a>
       </li>
     </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo base_url(); ?>posts/create">Create Post</a>
+      </li>
+    </ul>
+    </div>
   </div>
 </nav>
 
