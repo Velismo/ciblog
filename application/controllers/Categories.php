dbc8be6121cf3282389fb30bdbdd19ad8b7b434a
Juan Manuel Cantero Diego <juanmanuelcantero@gmail.com>
2020-11-12 19:05:24 +0100

    adding some new stuffs to categories

--- application/controllers/Categories.php
+++ application/controllers/Categories.php
@@ -1,5 +1,15 @@
 <?php
     class Categories extends CI_Controller{
+        public function index(){
+            $data['title'] = 'Categories';
+
+            $data['categories'] = $this->category_model->get_categories();
+
+            $this->load->view('templates/header');
+            $this->load->view('categories/index', $data);
+            $this->load->view('templates/footer');
+        }
+
         public function create(){
             $data['title'] = 'Create Category';
 
@@ -13,5 +23,15 @@
                 $this->category_model->create_category();
                 redirect('categories');
             }
+        }
+
+        public function posts($id){
+            $data['title'] = $this->category_model->get_category($id)->name;
+
+            $data['posts'] = $this->post_model->get_posts_by_category($id);
+
+            $this->load->view('templates/header');
+            $this->load->view('posts/index', $data);
+            $this->load->view('templates/footer');
         }
     }
