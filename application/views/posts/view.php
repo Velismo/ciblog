dbf9bd36c34cc1eeb7dbdfaee29c89a3e3cad1d3
Juan Manuel Cantero Diego <juanmanuelcantero@gmail.com>
2020-11-26 23:35:35 +0100

    finished the comment section

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -12,7 +12,21 @@
     <input type="submit" value="Delete" class="btn btn-danger">
 </form>
 <hr>
+<h3>Comments</h3>
+<br>
+<?php if($comments) : ?>
+    <?php foreach($comments as $comment) : ?>
+        <div class="card card-body bg-light">
+            <h5><?php echo $comment['body']; ?> [by <strong><?php echo $comment['name']; ?></strong>]</h5>
+        </div>
+        <br>
+    <?php endforeach; ?>
+    <?php else : ?>
+        <p>No comments to display</p>
+    <?php endif; ?>
+<hr>
 <h3>Add Comment</h3>
+<?php echo validation_errors();?>
 <?php echo form_open('comments/create/'.$post['id']); ?>
     <div class="form-group">
         <label>Name</label>
@@ -24,7 +38,7 @@
     </div>
     <div class="form-group">
         <label>Body</label>
-        <textarea name="name" class="form-control"></textarea>
+        <textarea name="body" class="form-control"></textarea>
     </div>
     <input type="hidden" name="slug" value="<?php echo $post['slug']; ?>">
     <button class="btn btn-info" type="submit">Submit</button>
